e6927760bb0fa69cb947a8d7251b7bc8bf805471
amirsep <amir.sep19@gmail.com>
2024-03-09 11:03:48 +0530

    minor update

---
+++ app/layout.jsx
@@ -0,0 +1,7 @@
+import "@styles/globals.css";
+
+const layout = () => {
+  return <div>layout</div>;
+};
+
+export default layout;
--- src/app/user/newUser/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>new user</div>;
-};
-
-export default page;
