48107dcd3ae19b8b27a593564d63f57291d6cdb5
amirsep <amir.sep19@gmail.com>
2024-03-10 00:04:09 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,18 @@
-import "@styles/globals.css";
+import "../styles/globals.css";
 
+export const metadata = {
+  title: "Promtopia",
+  description: "Discover &  Share AI Prompts",
+};
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    <html lang="en">
+      <body className="main">
+        <div className="bg-gradient" />
+      </body>
+      {/* <main className="app"></main> */}
+    </html>
+  );
 };
 
 export default layout;
