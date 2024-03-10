9528e5bacde7bb43c41949e228c6730b4ca76cc4
amirsep <amir.sep19@gmail.com>
2024-03-10 18:10:58 +0530

    update

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+let isConnected = false; //track the connection
+
+export const connectedToDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("MongoDB is already Connected");
+    retrun;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
