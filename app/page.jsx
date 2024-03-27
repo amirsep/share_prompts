db60478f183a8397e16825a83d5bfad72128f8d0
amirsep <amir.sep19@gmail.com>
2024-03-27 15:05:43 +0530

    update

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           </span>
         </h1>
         <p className="desc text-center">
-          Promptopia is an open-source AI prompting tool for modern world to
+          Promptopia is an Open-Source AI prompting tool for modern world to
           discover, create and share creative prompts
         </p>
         <Feed />
